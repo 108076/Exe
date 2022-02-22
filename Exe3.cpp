0e6d685eea950bbc7495bfaea0d4a4a4eaf7269f
108076 <100131522+108076@users.noreply.github.com>
2022-02-22 20:07:40 +0800

    Create Exe3.cpp

---
+++ Exe3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myAge = 15;
+    printf(" I am %d ", myAge);
+     cout <<  "my height is " << 1 << "m " << 80 << "cm";
+
+    return 0;
+     printf
+     ;cout <<  "my weight is " << 40 << "kg";
+    
+    return 0;
+    
+}
